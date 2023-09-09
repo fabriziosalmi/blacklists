46de0bd5f93e4ce3881289f24628f5923aca08ca
fab <fabrizio.salmi@gmail.com>
2023-09-09 20:45:44 +0200

    Update sanitize.py

--- sanitize.py
+++ sanitize.py
@@ -7,65 +7,55 @@ fqdn_pattern = re.compile('^(?!-)[A-Za-z0-9-]{1,63}(?<!-)$')
 
 def is_valid_fqdn(s):
     """Check if the string is a valid FQDN."""
-    if '*' in s:
+    if '*' in s or not s:
         return False
     extracted = tldextract.extract(s)
     if not all([extracted.domain, extracted.suffix]):
         return False
     return all(fqdn_pattern.match(x) for x in s.split('.'))
 
-def remove_comment(line):
-    """Remove comment lines that start with '#'."""
-    return None if line.startswith("#") else line
+def remove_prefix(line, prefix):
+    """General function to remove specified prefix from a line."""
+    if line.startswith(prefix):
+        potential_fqdn = line[len(prefix):]
+        if is_valid_fqdn(potential_fqdn):
+            return potential_fqdn
+    return line
 
-def remove_ip_prefix_127(line):
-    """Remove the IP prefix '127.0.0.1 '."""
-    return line.replace("127.0.0.1 ", "", 1) if line.startswith("127.0.0.1 ") else line
-
-def remove_ip_prefix_0(line):
-    """Remove the IP prefix '0.0.0.0 '."""
-    return line.replace("0.0.0.0 ", "", 1) if line.startswith("0.0.0.0 ") else line
-
-def remove_double_pipe(line):
-    """Remove lines that start with '||'."""
-    return line.replace("||", "", 1) if line.startswith("||") else line
-
-def remove_http(line):
-    """Remove lines that start with 'http://'."""
-    return line.replace("http://", "", 1) if line.startswith("http://") else line
-
-def remove_https(line):
-    """Remove lines that start with 'https://'."""
-    return line.replace("https://", "", 1) if line.startswith("https://") else line
-
-# List of sanitization rules
-sanitization_rules = [
-    remove_comment,
-    remove_ip_prefix_127,
-    remove_ip_prefix_0,
-    remove_double_pipe,
-    remove_http,
-    remove_https,
-]
-
-def sanitize_line(line):
+def sanitize_line(line, rules):
     """Apply all sanitization rules to a line."""
-    line = line.strip()
-    for rule in sanitization_rules:
-        line = rule(line)
+    for rule in rules:
+        line = rule(line.strip())
         if line is None:
             return None
-    return line if is_valid_fqdn(line) else None
+    return line
+
+def get_sanitization_rules():
+    """Returns a list of sanitization rules."""
+    return [
+        lambda line: None if line.startswith("#") else line,       # Remove comment lines
+        lambda line: remove_prefix(line, "127.0.0.1"),             # Remove IP prefix 127.0.0.1 without space
+        lambda line: remove_prefix(line, "127.0.0.1 "),            # Remove IP prefix 127.0.0.1 with space
+        lambda line: remove_prefix(line, "0.0.0.0"),               # Remove IP prefix 0.0.0.0 without space
+        lambda line: remove_prefix(line, "0.0.0.0 "),              # Remove IP prefix 0.0.0.0 with space
+        lambda line: remove_prefix(line, "||"),                    # Remove double pipes
+        lambda line: remove_prefix(line, "http://"),               # Remove http prefix
+        lambda line: remove_prefix(line, "https://"),              # Remove https prefix
+        lambda line: line.rstrip('.'),                             # Remove trailing dot
+        lambda line: line.lower()                                  # Convert to lowercase
+    ]
 
 def process_large_file(input_file_path, output_file_path):
     """Process large files line by line."""
     unique_domains = set()
-    total_lines = sum(1 for _ in open(input_file_path))
+    rules = get_sanitization_rules()
 
     with open(input_file_path, 'r') as infile:
+        total_lines = sum(1 for _ in infile)
+        infile.seek(0)  # Reset file pointer to start
         for line in tqdm(infile, total=total_lines, desc="Processing"):
-            sanitized_line = sanitize_line(line)
-            if sanitized_line is not None:
+            sanitized_line = sanitize_line(line, rules)
+            if sanitized_line and is_valid_fqdn(sanitized_line):
                 unique_domains.add(sanitized_line)
 
     # Sort the unique domain names in alphabetical order
